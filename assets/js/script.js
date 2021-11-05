f71fb2576e466d98caed993e912043c11b3f2bc4
Vova <v2110vova@gmail.com>
2021-11-05 02:12:22 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-let indexx;
-
 function Slide(index, title, background, link ) {
     this.title = title;
     this.background = background;
@@ -19,6 +17,7 @@ function Slide(index, title, background, link ) {
         this.buildSlider();
     },
     buildSlider: function() {
+
         let sliderHTML = "";
         for(let slide of this.slides) {
  //зверніть увагу на можливість використання ``,яка дозволяє додавати в string змінні ${}
@@ -28,10 +27,24 @@ function Slide(index, title, background, link ) {
             <div class='slideOverlay'>
             <h2>${slide.title}</h2>
             <a class='link' href='${slide.link}' target='_blank'>Open</a></div></div>`;
+
+        const targetDiv = document.getElementById("slides");
+
+        let btn = document.createElement('button');
+        btn.className = 'slides';
+        btn.id = 'button-'+slide.id;
+        btn.innerHTML = slide.id;    
+        btn.addEventListener("click", function(e) {
+          Slider.setSlide(slide.id);
+        })
+
+        targetDiv.appendChild(btn); 
         }
+        
         document.getElementById("slider").innerHTML = sliderHTML;
         document.getElementById("slide-" + this.current).style.left = 0;
     },
+
     prevSlide: function() {
         let next;
         if (this.current === 0 ) {
@@ -64,17 +77,60 @@ function Slide(index, title, background, link ) {
  
         this.current = next;
     },
-     setSlide: function (id) {
-         console.log("ID " + id + "INDEXX " + indexx);
-         if (indexx !== id) {
-             if (id > indexx) {
-                 this.prevSlide();
-                 this.setSlide(id);
-             }
-             else {
-                 this.nextSlide();
-                 this.setSlide(id);
-             }
-         }
-     },
+
+    setSlide: function (id) {
+        const nextNumber = Number(id.replace("slide-", ""));
+    
+        const diff = nextNumber - this.current;
+        if(diff === 0) return;
+        else if(diff > 0) {
+          for (let i = 0; i < diff; i++) {
+            Slider.nextSlide();
+          }
+        } else if (diff < 0) {
+          for (let i = 0; i < Math.abs(diff); i++) {
+            Slider.prevSlide();
+          }
+        }
+    },
  }
+
+
+ const leftControl = document.querySelector('.slid-left'); 
+ const rightControl = document.querySelector('.slid-right');
+ 
+ leftControl.addEventListener('click', () => {
+     Slider.prevSlide();
+ })
+ 
+ rightControl.addEventListener('click', () => {
+     Slider.nextSlide();
+ })
+ 
+const hideContorol = document.querySelector('.slid-hide');
+const showControl = document.querySelector('.slid-show');
+const slider = document.querySelector('.slider');
+
+hideContorol.addEventListener('click', () => {
+    slider.classList.add('hide');
+})
+
+showControl.addEventListener('click', () => {
+    slider.classList.remove('hide');
+})
+
+const startStop = document.querySelector('.slide-startStop');
+
+startStop.addEventListener('click', (event)=>{
+    if (event.target.classList.contains('start')){
+        event.target.innerHTML = 'Start';
+        clearInterval(interval)
+    } else {
+        event.target.innerHTML = 'Stop';
+        interval = setInterval(()=>{
+            Slider.nextSlide();
+        },1000)
+    }
+    event.target.classList.toggle('start')
+ })
+ 
